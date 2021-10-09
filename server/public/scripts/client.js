36378b5720103fc9f94b191129de3b1fdeb29476
John Gaffaney <87045628+jgaffaney@users.noreply.github.com>
2021-10-09 16:13:39 -0500

    equals button added and wired

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,8 +1,28 @@
-console.log('JS');
-
 $(readyNow);
 
 function readyNow() {
-    console.log('JQ');
+
+    // render();
+
+    // click listeners
+    // $('#addition').on('click', addAddition);
+    // $('#subtract').on('click', addSubtract);
+    // $('#multily').on('click', addMultily);
+    // $('#divide').on('click', adddivide);
+    $('#compute').on('click', compute);
+}
+
+// a function to send inputs and operator to server for processing
+function compute(operator) {
+    console.log('equals clicked');
     
+    // $.ajax({
+    //     method: 'POST',
+    //     url: '/compute',
+    //     data: {
+    //         firstNumber: $('#firstNumberIn').val(),
+    //         secondNumber: $('#secondNumberIn').val(),
+    //         operator: $
+    //     }
+    // })
 }
