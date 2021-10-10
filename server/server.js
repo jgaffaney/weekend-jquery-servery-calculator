ddc1e12682475774f45958c0c21ca48488287784
John Gaffaney <87045628+jgaffaney@users.noreply.github.com>
2021-10-10 09:10:49 -0500

    calculate function

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = 5000;
 
+let resultsArray = [];
+
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("server/public"));
@@ -10,3 +12,29 @@ app.use(express.static("server/public"));
 app.listen(PORT, () => {
     console.log('Server is running on port ', PORT);    
 })
+
+app.post('/calculate', (req, res) => {
+    console.log('/calculate POST req.body ', req.body);
+    
+    
+})
+
+function calculate(input) {
+    let result = 0;
+    switch (input.operator) {
+        case '+':
+            result = Number(input.firstNumber) + Number(input.secondNumber);
+        break;
+        case '-':
+            result = Number(input.firstNumber) - Number(input.secondNumber);
+        break;
+        case '*':
+            result = Number(input.firstNumber) * Number(input.secondNumber);
+        break;
+        case '/':
+            result = Number(input.firstNumber) / Number(input.secondNumber);
+        break;
+    }
+console.log(result);
+
+} 
